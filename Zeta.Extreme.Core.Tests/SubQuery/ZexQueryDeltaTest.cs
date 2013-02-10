aebae005cdd957195f05f0df9ea609e31d39094c
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-02-10 23:31:25 +0600

    Добавлен базвый класс дельты, проверена функциональность и базовая производительность

---
+++ Zeta.Extreme.Core.Tests/SubQuery/ZexQueryDeltaTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Zeta.Extreme.Core.Tests.SubQuery
+{
+	[TestFixture]
+	public class ZexQueryDeltaTest
+	{
+		[Test]
+		public void RowMove() {
+			var q = new ZexQuery {Row = {Code = "X"}};
+			var d = new ZexQueryDelta {RowCode = "Y"};
+			var dq = d.Apply(q);
+			Assert.AreNotSame(q,dq);
+			Assert.AreSame(q.Col,dq.Col);
+			Assert.AreNotSame(q.Row,dq.Row);
+			Assert.AreEqual("Y",dq.Row.Code);
+		}
+		[Test]
+		public void ColMove()
+		{
+			var q = new ZexQuery { Col = { Code = "X" } };
+			var d = new ZexQueryDelta { ColumCode = "Y" };
+			var dq = d.Apply(q);
+			Assert.AreNotSame(q, dq);
+			Assert.AreSame(q.Row, dq.Row);
+			Assert.AreNotSame(q.Col, dq.Col);
+			Assert.AreEqual("Y", dq.Col.Code);
+		}
+
+		[Test]
+		public void ObjMove()
+		{
+			var q = new ZexQuery { Obj = { Id = 24 } };
+			var d = new ZexQueryDelta { ObjId = 35 };
+			var dq = d.Apply(q);
+			Assert.AreNotSame(q, dq);
+			Assert.AreSame(q.Row, dq.Row);
+			Assert.AreNotSame(q.Obj, dq.Obj);
+			Assert.AreEqual(35, dq.Obj.Id);
+		}
+
+		[Test]
+		public void YearMove()
+		{
+			var q = new ZexQuery { Time = { Year = 2012 } };
+			var d = new ZexQueryDelta { Year = 1 };
+			var dq = d.Apply(q);
+			Assert.AreNotSame(q, dq);
+			Assert.AreSame(q.Row, dq.Row);
+			Assert.AreNotSame(q.Time, dq.Time);
+			Assert.AreEqual(2013, dq.Time.Year);
+		}
+
+		[Test]
+		public void YearPeriodMove()
+		{
+			var q = new ZexQuery { Time = { Year = 2012 } };
+			var d = new ZexQueryDelta { Year = 2014,Period = 1};
+			var dq = d.Apply(q);
+			Assert.AreNotSame(q, dq);
+			Assert.AreSame(q.Row, dq.Row);
+			Assert.AreNotSame(q.Time, dq.Time);
+			Assert.AreEqual(2014, dq.Time.Year);
+			Assert.AreEqual(1, dq.Time.Period);
+		}
+
+		[Test]
+		[Explicit]
+		public void BenchMarkTest() {
+			var sw = Stopwatch.StartNew();
+			int i=0;
+			for(var r = 0;r<500;r++) { //rowcodes
+				for(var c = 0;c<10;c++) { //колонки
+					for(var o=1;o<15;o++) { //объекты
+						for (var y=2010;y<2014;y++) { //годы
+							for(var p=0;p<10;p++) { //периоды
+								i++;
+								var q = new ZexQuery
+									{
+										Row = {Code = r.ToString()},
+										Col = {Code = c.ToString()},
+										Obj = {Id = o},
+										Time = {Year = y, Period = p}
+									};
+								var d = new ZexQueryDelta
+									{
+										RowCode = r + "+",
+										ColumCode = c + "+",
+										ObjId = o + 10,
+										Year = y + 1,
+										Period = p + 1
+									};
+								q = d.Apply(q);
+								//Assert.AreEqual(r+"+",q.Row.Code);
+								//Assert.AreEqual(c + "+", q.Col.Code);
+								//Assert.AreEqual(o + 10, q.Obj.Id);
+								//Assert.AreEqual(y+1,q.Time.Year);
+								//Assert.AreEqual(p + 1, q.Time.Period);
+							}
+						}
+					}
+				}
+			}
+			sw.Stop();
+			Console.WriteLine(i);
+			Console.WriteLine(sw.Elapsed);
+			Console.WriteLine(sw.ElapsedMilliseconds/(double)i);
+		}
+	}
+}
---
+++ Zeta.Extreme.Core/Query/ZexQueryDelta.cs
@@ -0,0 +1,190 @@
+﻿#region LICENSE
+
+// Copyright 2012-2013 Media Technology LTD 
+// Solution: Qorpent.TextExpert
+// Original file : SubQueryDescriptor.cs
+// Project: Zeta.Extreme.Core
+// This code cannot be used without agreement from 
+// Media Technology LTD 
+
+#endregion
+
+using System;
+using Comdiv.Zeta.Data.Minimal;
+using Comdiv.Zeta.Model;
+
+namespace Zeta.Extreme {
+	/// <summary>
+	/// 	Описывает потенциальный подзапрос для оптимизации расчета сумм и простых формул
+	/// </summary>
+	public sealed class ZexQueryDelta {
+		/// <summary>
+		/// 	Применяет смещение к целевому запросу
+		/// </summary>
+		/// <param name="target"> </param>
+		/// <returns> </returns>
+		public ZexQuery Apply(ZexQuery target) {
+			if (NoChanges(target)) {
+				return target;
+			}
+			var result = target.Copy();
+			MoveColumn(result);
+			MoveRow(result);
+			MoveObj(result);
+			MoveTime(result);
+			return result;
+		}
+
+		private void MoveTime(ZexQuery result) {
+			if ((Year != 0 && Year != result.Time.Year) || (Period != 0 && Period != result.Time.Period)) {
+				result.Time = result.Time.Copy();
+				if (0 != Year && Year < 1900) {
+					result.Time.Year += Year;
+				}else if(Year!=result.Time.Year) {
+					result.Time.Year = Year;
+				}
+				if (0 != Period) {
+					if (0 < Period) {
+						result.Time.Period = Period;
+					}
+					else {
+						var eval = Periods.Eval(result.Time.Year, result.Time.Period, Period);
+						result.Time.Year = eval.Year;
+						result.Time.Period = eval.Periods[0];
+					}
+				}
+			}
+		}
+
+		private void MoveColumn(ZexQuery result) {
+			if (null != Column) {
+				if (Column != result.Col.Native) {
+					result.Col = new ColumnHandler {Native = Column};
+				}
+			}
+			else if (!string.IsNullOrWhiteSpace(ColumCode)) {
+				if (ColumCode != result.Col.Code) {
+					result.Col = result.Col.Copy();
+					if (null != result.Col.Native) {
+						result.Col.Native = null;
+					}
+					result.Col.Code = ColumCode;
+				}
+			}
+		}
+
+		private void MoveRow(ZexQuery result) {
+			if (null != Row) {
+				if (Row != result.Row.Native) {
+					result.Row = new RowHandler {Native = Row};
+				}
+			}
+			else if (!string.IsNullOrWhiteSpace(RowCode)) {
+				if (RowCode != result.Row.Code) {
+					result.Row = result.Row.Copy();
+					if (null != result.Row.Native) {
+						result.Row.Native = null;
+					}
+					result.Row.Code = RowCode;
+				}
+			}
+		}
+
+		private void MoveObj(ZexQuery result) {
+			if (null != Obj) {
+				if (Obj != result.Obj.Native) {
+					result.Obj = new ObjHandler {Native = Obj};
+				}
+			}
+			else if (0 != ObjId) {
+				if (ObjId != result.Obj.Id) {
+					result.Obj = result.Obj.Copy();
+					if (null != result.Obj.Native) {
+						result.Obj.Native = null;
+					}
+					result.Obj.Id = ObjId;
+				}
+			}
+		}
+
+
+		private bool NoChanges(ZexQuery target) {
+			if (0 > Period) {
+				return false; //формульный период
+			}
+			if (0 != Year && Math.Abs(Year) < 1900) {
+				return false; //смещение года
+			}
+			if (null != Column && Column != target.Col.Native) {
+				return false;
+			}
+			if (null != Row && Row != target.Row.Native) {
+				return false;
+			}
+			if (null != Obj && Obj != target.Obj.Native) {
+				return false;
+			}
+			if (!string.IsNullOrWhiteSpace(ColumCode) && ColumCode != target.Col.Code) {
+				return false;
+			}
+			if (!string.IsNullOrWhiteSpace(RowCode) && RowCode != target.Row.Code) {
+				return false;
+			}
+			if (0 != ObjId && ObjId != target.Obj.Id) {
+				return false;
+			}
+			if (0 != Period && Period != target.Time.Period) {
+				return false;
+			}
+			if (0 != Year && Year != target.Time.Year) {
+				return false;
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// 	Смещение по колонке (код) или пустое значение для отсутствия смещения
+		/// </summary>
+		public string ColumCode;
+
+		/// <summary>
+		/// 	Смещение по колонке (прямое) или null
+		/// </summary>
+		public IZetaColumn Column;
+
+		/// <summary>
+		/// 	Множитель при расчете значений
+		/// </summary>
+		public decimal Multiplicator;
+
+		/// <summary>
+		/// 	Прямое смещение по объекту
+		/// </summary>
+		public IZetaMainObject Obj;
+
+		/// <summary>
+		/// 	Смещение по объекту
+		/// </summary>
+		public int ObjId;
+
+		/// <summary>
+		/// 	Прямое или дельтированное указание периода
+		/// </summary>
+		public int Period;
+
+		/// <summary>
+		/// 	Прямое укзание смещения строки или отсутствие
+		/// </summary>
+		public IZetaRow Row;
+
+		/// <summary>
+		/// 	Смещение по строке или пустое значение для отсутствия смещения
+		/// </summary>
+		public string RowCode;
+
+		/// <summary>
+		/// 	Прямое или дельтированое указание года
+		/// </summary>
+		public int Year;
+	}
+}
