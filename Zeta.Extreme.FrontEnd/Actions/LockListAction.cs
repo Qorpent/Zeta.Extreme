692c6a7a4ab3577b34649834b7f6c902154c9b3d
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-03-01 00:27:41 +0600

    file struct fix

---
+++ Zeta.Extreme.FrontEnd/Actions/LockListAction.cs
@@ -0,0 +1,19 @@
+using Qorpent.Mvc;
+
+namespace Zeta.Extreme.FrontEnd {
+	/// <summary>
+	/// Возвращает историю блокировок
+	/// </summary>
+	[Action("zefs.locklist")]
+	public class LockListAction : FormSessionActionBase
+	{
+		/// <summary>
+		/// 	processing of execution - main method of action
+		/// </summary>
+		/// <returns> </returns>
+		protected override object MainProcess()
+		{
+			return MySession.GetLockHistory();
+		}
+	}
+}
