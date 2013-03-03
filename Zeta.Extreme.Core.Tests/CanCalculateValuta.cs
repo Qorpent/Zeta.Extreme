07e5664afd5b3eed8cb9ee3040f13e61f76cb56a
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-03-03 22:35:32 +0600

    valuta counting tested

---
+++ Zeta.Extreme.Core.Tests/CanCalculateValuta.cs
@@ -0,0 +1,35 @@
+using System;
+using NUnit.Framework;
+using Zeta.Extreme.Core.Tests.CoreTests;
+
+namespace Zeta.Extreme.Core.Tests {
+	[TestFixture]
+	public class CanCalculateValuta : SessionTestBase
+	{
+		[Test]
+		public void SimpleUsdCalculation()
+		{
+			var query = new Query
+				{
+					Row = { Code = "m260113" },
+					Col = { Code = "Á1" },
+					Obj = { Id = 352, Type = ObjType.Obj },
+					Time = { Year = 2012, Period = 1 }
+				}
+				;
+			var result = _serial.Eval(query);
+			var rubresult = result.NumericResult;
+			query = new Query
+				{
+					Row = {Code = "m260113"},
+					Col = {Code = "Á1"},
+					Obj = {Id = 352, Type = ObjType.Obj},
+					Time = {Year = 2012, Period = 1},
+					Valuta = "USD",
+				};
+			result = _serial.Eval(query);
+			const decimal usdrate = 29.32820m;
+			Assert.AreEqual(Math.Round(rubresult /usdrate, 6), result.NumericResult);
+		}
+	}
+}
