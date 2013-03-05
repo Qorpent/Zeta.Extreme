5f718456c1b02868cf870422d16c3c4f96de08c7
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-03-05 10:32:20 +0600

    action для информации о пользователе

---
+++ Zeta.Extreme.FrontEnd/Helpers/SimpleUserInfo.cs
@@ -0,0 +1,46 @@
+namespace Zeta.Extreme.FrontEnd.Helpers {
+	/// <summary>
+	/// Упрощенный описатель пользователя
+	/// </summary>
+	public class SimpleUserInfo {
+		/// <summary>
+		/// Логин
+		/// </summary>
+		public string Login { get; set; }
+		/// <summary>
+		/// Имя
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// Должность
+		/// </summary>
+		public string Dolzh { get; set; }
+
+		/// <summary>
+		/// Контактные данные
+		/// </summary>
+		public string Contact { get; set; }
+		/// <summary>
+		/// Электронная почта
+		/// </summary>
+		public string Email { get; set; }
+		/// <summary>
+		/// Идентификатор предприятия
+		/// </summary>
+		public int ObjId { get; set; }
+		/// <summary>
+		/// Имя предприятия
+		/// </summary>
+		public string ObjName { get; set; }
+		/// <summary>
+		/// Признак администратора предприятия
+		/// </summary>
+		public bool IsObjAdmin { get; set; }
+
+		/// <summary>
+		/// Признак активности пользователя
+		/// </summary>
+		public bool Active { get; set; }
+
+	}
+}
