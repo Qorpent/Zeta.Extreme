22703a3c29bd7e383bcc941c221aa768a9ab7ab3
comdiv <fagim.sadykov@gmail.com>
2013-03-07 20:46:35 +0600

    start create tests for analyzer

---
+++ Zeta.Extreme.Analyzer.Tests/FormulaTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Zeta.Extreme.Analyzer.Tests
+{
+    [TestFixture]
+    public class FormulaTests
+    {
+        public const string DEFAULT_SIMPLE_FORMULA = "$CODE1@COL1.P3? + $CODE2@COL3? - $code1@COl2?";
+        [Test]
+        public void CanCreateFormula()
+        {
+            var formula = new Formula(DEFAULT_SIMPLE_FORMULA);
+        }
+    }
+}
---
+++ Zeta.Extreme.Analyzer/Formula.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zeta.Extreme.Analyzer
+{
+    /// <summary>
+    /// Инкапсуляция формулы измерения (строки, колнки, объекта)
+    /// </summary>
+    public class Formula
+    {
+        private string _definition;
+        /// <summary>
+        /// Создает формулу с указанным строчным определением
+        /// </summary>
+        /// <param name="definition"></param>
+        public Formula(string definition)
+        {
+            // TODO: Complete member initialization
+            this._definition = definition;
+        }
+    }
+}
