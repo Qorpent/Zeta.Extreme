25e996217fc40d983a8a84383d3c1b55d807563e
Kirill Shirshov <remalloc@gmail.com>
2013-03-08 19:00:02 +0600

    Some steps to solve the P13-3

--- Zeta.Extreme.Form.Tests/MongoDBAttachmentSource/CanOpenStream.cs
+++ Zeta.Extreme.Form.Tests/MongoDBAttachmentSource/CanOpenStream.cs
@@ -11,14 +11,20 @@ namespace Zeta.Extreme.Form.Tests.MongoDBAttachmentSource {
     public class CanOpenStream {
 
         public string CanOpen() {
-            //MongoDBAttachment mdba = new MongoDBAttachment();
+            MongoDBAttachment mdba = new MongoDBAttachment();
             Attachment ex_att = new Attachment();
             
             ex_att.Uid = "Some_UID";
             ex_att.Comment = "Some_comment";
+            ex_att.Name = "filename";
+            ex_att.Size = 111;
+            ex_att.Type = "fsffsdf";
+            ex_att.User = "rrr";
+            ex_att.MimeType = "addsd";
+            ex_att.Extension = "some";
+             
+            mdba.Save(ex_att);
             return "Going Ok.";
-
-           // mdba.Open(ex_att, FileAccess.ReadWrite);
         }
 
         
--- Zeta.Extreme.Form/MongoDBAttachmentSource/MongoDBAttachmentSource.cs
+++ Zeta.Extreme.Form/MongoDBAttachmentSource/MongoDBAttachmentSource.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.Data;
+using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using Qorpent.Applications;
 using Zeta.Extreme.BizProcess.Forms;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using Comdiv.Extensions;
+
 
 namespace Zeta.Extreme.Form.MongoDBAttachmentSource {
+
+    class AttachmentDescription : Attachment {
+        public string tag {
+            get;
+            set;
+        }
+
+        public string extension {
+            get;
+            set;
+        }
+    }
+
     class MongoDBAttachment : IAttachmentStorage {
+        private const string _DEFAULT_DB_NAME = "attachments";
+        private const string _DEFAULT_FILE_NAME = "noname";
+        private const string _SAVE_QUERY_SKELETON = "";
+
+        MongoClient client;
+        MongoServer server;
+        MongoDatabase database;
+        private BsonDocument CurrentFile;
 
         public IEnumerable<Attachment> Find(Attachment query) {
             return null;
@@ -16,20 +41,59 @@ namespace Zeta.Extreme.Form.MongoDBAttachmentSource {
         public void Delete(Attachment attachment) {
 
         }
-        public void Save(Attachment attachment) {
 
+        public void Save(Attachment attachment) {
+            this.HandleVariables(attachment);
+            this.MongoConnect();
+            this.SaveAttachmentView();
         }
-        /// <summary>
-        /// Открытие подключения к MongoDB
-        /// </summary>
-        /// <param name="attachment"></param>
-        /// <param name="mode"></param>
-        /// <returns></returns>
+
         public Stream Open(Attachment attachment, FileAccess mode) {
-            //MongoClient client = new MongoClient();
-            Console.Write("{0}, {1}", attachment.Uid, attachment.Comment);
             return null;
         }
+        /// <summary>
+        /// Init the connection to the database
+        /// </summary>
+        private void MongoConnect() {
+            this.client = new MongoClient();
+            this.server = client.GetServer();
+            this.database = server.GetDatabase("db");
+        }
+        /// <summary>
+        /// Saving the attachement description to the database
+        /// </summary>
+        private void SaveAttachmentView() {
+            this.database.GetCollection("AttachmentView").Insert(this.CurrentFile);
+        }
 
+        private void HandleEmptyVariables() {
+
+        }
+
+        /// <summary>
+        /// Функция обрабатывает входные параметры
+        /// </summary>
+        /// <param name="attachment">Входные параметры описания файла</param>
+        /// <returns>Возвращает переформированные данные описания файла</returns>
+        private void HandleVariables(Attachment attachment) {
+            this.CurrentFile = new BsonDocument();
+
+            this.HandleEmptyVariables();
+
+            this.CurrentFile["type"] = attachment.Type;
+            this.CurrentFile["Comment"] = attachment.Comment ?? "";
+            this.CurrentFile["User"] = attachment.User ?? Application.Current.Principal.CurrentUser.Identity.Name;
+            this.CurrentFile["MimeType"] = attachment.MimeType ?? "unknown/bin";
+
+            this.CurrentFile["tag"] = TagHelper.ToString(
+                    attachment.Metadata.ToDictionary(
+                        _ => _.Key == "template" ? "form" : _.Key,
+                        _ => _.Value.ToString()
+                    )
+            );
+
+            this.CurrentFile["tag"] = TagHelper.Merge(attachment.Type, "/doctype:" + attachment.Type + "/");
+            this.CurrentFile["extension"] = attachment.Extension;
+        }
     }
 }
