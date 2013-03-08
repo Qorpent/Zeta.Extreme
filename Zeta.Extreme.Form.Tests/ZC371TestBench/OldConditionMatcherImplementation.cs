c5c97ba3f507894fdbefc36021f44c7a8620bc86
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-03-08 23:41:33 +0600

    #ZC-371 fixed all test framework ready

--- Zeta.Extreme.Form.Tests/ZC371TestBench/OldConditionMatcherImplementation.cs
+++ Zeta.Extreme.Form.Tests/ZC371TestBench/OldConditionMatcherImplementation.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using Comdiv.Extensibility;
 using Comdiv.Extensions;
+using Qorpent.Dsl.LogicalExpressions;
 using Qorpent.Utils.Extensions;
 
 namespace Zeta.Extreme.Form.Tests.ZC371TestBench
 {
 
 	public class NewConditionMatcherImplementation : ConditionMatcherBase {
+		readonly LogicalExpressionEvaluator _evaluator = new LogicalExpressionEvaluator();
 		protected override bool EvaluateByScript(string condition, IEnumerable<string> conds) {
-			throw new NotImplementedException();
+			var normalizedCondition = condition.Replace(" and ", " & ").Replace(" or ", " | ").Replace(" not ", " ! ");
+			var soruce = LogicTermSource.Create(conds);
+			return _evaluator.Eval(normalizedCondition, soruce);
 		}
 
 		protected override bool EvaluateByListLikeCondition(string condition, IEnumerable<string> conds) {
