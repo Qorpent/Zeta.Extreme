2d7fa234d60e50f8a86ca7996c768ec2c2d52526
Kirill Shirshov <remalloc@gmail.com>
2013-03-08 13:29:14 +0600

    Begin of implementation the IAttachmentStorage for MongoDB

--- Zeta.Extreme.Form/MongoDBAttachmentSource/MongoDBAttachmentSource.cs
+++ Zeta.Extreme.Form/MongoDBAttachmentSource/MongoDBAttachmentSource.cs
@@ -1,8 +1,34 @@
-﻿using Qorpent.Applications;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Qorpent.Applications;
 using Zeta.Extreme.BizProcess.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace Zeta.Extreme.Form.MongoDBAttachmentSource {
-    class MongoDBAttachmentSource : IAttachmentStorage {
+    class MongoDBAttachment : IAttachmentStorage {
+
+        public IEnumerable<Attachment> Find(Attachment query) {
+            return null;
+        }
+
+        public void Delete(Attachment attachment) {
+
+        }
+        public void Save(Attachment attachment) {
+
+        }
+        /// <summary>
+        /// Открытие подключения к MongoDB
+        /// </summary>
+        /// <param name="attachment"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public Stream Open(Attachment attachment, FileAccess mode) {
+            MongoClient client = new MongoClient();
+            return null;
+        }
 
     }
 }
