9ff09add66e6137d28ecc7ac9db02c3bbc878963
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-03-09 00:01:29 +0600

    #ZC-373 fixed

--- Zeta.Extreme.Form.Tests/ZC371TestBench/OldConditionMatcherImplementation.cs
+++ Zeta.Extreme.Form.Tests/ZC371TestBench/OldConditionMatcherImplementation.cs
@@ -12,9 +12,11 @@ namespace Zeta.Extreme.Form.Tests.ZC371TestBench
 	public class NewConditionMatcherImplementation : ConditionMatcherBase {
 		readonly LogicalExpressionEvaluator _evaluator = new LogicalExpressionEvaluator();
 		protected override bool EvaluateByScript(string condition, IEnumerable<string> conds) {
-			var normalizedCondition = condition
-				.Replace(" and ", " & ").Replace(" or ", " | ").Replace(" not ", " ! ")
-				.Replace("(and ", "(& ").Replace("(or ", "(| ").Replace("(not ", "(! ");
+			var normalizedCondition = (" " + condition + " ").Replace("(", " ( ").Replace(")", " ) ")
+				//fix not processable formulas
+				.Replace(" and ", " & ").Replace(" or ", " | ").Replace(" not ", " ! "); 
+				//fix operators
+				
 			var soruce = LogicTermSource.Create(conds);
 			return _evaluator.Eval(normalizedCondition, soruce);
 		}
