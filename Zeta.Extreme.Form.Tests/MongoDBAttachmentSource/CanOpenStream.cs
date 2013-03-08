bdc5749f4ad6c357ee83dbc86a7a3839d493831f
Kirill Shirshov <remalloc@gmail.com>
2013-03-08 14:27:36 +0600

    Zeta.Extreme.Form.Tests for MongoDB implementation updated

--- Zeta.Extreme.Form.Tests/MongoDBAttachmentSource/CanOpenStream.cs
+++ Zeta.Extreme.Form.Tests/MongoDBAttachmentSource/CanOpenStream.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Zeta.Extreme.Form.MongoDBAttachmentSource;
-
+using Zeta.Extreme.BizProcess.Forms;
 
 namespace Zeta.Extreme.Form.Tests.MongoDBAttachmentSource {
-    class CanOpenStream {
-        MongoDBAttachment db = new MongoDBAttachment();
+    public class CanOpenStream {
+
+        public string CanOpen() {
+            //MongoDBAttachment mdba = new MongoDBAttachment();
+            Attachment ex_att = new Attachment();
+            
+            ex_att.Uid = "Some_UID";
+            ex_att.Comment = "Some_comment";
+            return "Going Ok.";
+
+           // mdba.Open(ex_att, FileAccess.ReadWrite);
+        }
+
+        
     }
 }
