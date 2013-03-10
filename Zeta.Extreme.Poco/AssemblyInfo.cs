d7e7c8ad4a4f51e4119495f239eaf9da8bf17373
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-03-10 09:35:56 +0600

    poco normalized

--- Zeta.Extreme.Poco/AssemblyInfo.cs
+++ Zeta.Extreme.Poco/AssemblyInfo.cs
@@ -10,8 +10,5 @@
 
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
 
-[assembly: AssemblyTitle("Default Zeta model implementation")]
 [assembly: AssemblyDescription("Classes for ORM between Zeta3 class schema and zeta2 database")]
