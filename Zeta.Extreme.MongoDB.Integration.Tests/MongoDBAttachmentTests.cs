2b24ee108d975c2f81fc641a80f6eb02dfc34e2e
Kirill Shirshov <remalloc@gmail.com>
2013-03-11 20:20:04 +0600

    MongoDB.Integration for attachments update

---
+++ Zeta.Extreme.MongoDB.Integration.Tests/MongoDBAttachmentTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeta.Extreme.BizProcess.Forms;
+
+namespace Zeta.Extreme.MongoDB.Integration.Tests {
+    public class MongoDBAttachmentTests {
+        private MongoDBAttachmentsSource mdb;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MongoDBAttachmentTests() {
+            this.mdb = new MongoDBAttachmentsSource();
+        }
+
+        public void CanSave() {
+            var source = new byte[] { 84, 101, 115, 116, 32, 79, 75, 33 };
+            var attachment = new FormAttachment {
+                Uid = "Test_OK2",
+                Name = "Test OK File",
+                Type = "mdb-test"
+            };
+
+            this.mdb.Save(attachment);
+            using (var stream = this.mdb.Open(attachment, FileAccess.Write)) {
+                stream.Write(source, 0, source.Length);
+                stream.Flush();
+            }
+        }
+
+        public void CanDelete() {
+            var attachment = new FormAttachment {
+                Uid = "Test_OK2",
+                Name = "Test OK File",
+                Type = "mdb-test"
+            };
+
+            this.mdb.Delete(attachment);
+        }
+
+
+        public void CanFind() {
+            var attachment = new FormAttachment {                           // Test attachment description
+                Uid = "Test_OK",
+                Name = "Test OK File",
+                Type = "mdb-test"
+            };
+
+            this.mdb.Find(attachment);
+        }
+    }
+}
---
+++ Zeta.Extreme.MongoDB.Integration/MongoDBAttachmentsSource.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.GridFS;
+
+using Zeta.Extreme.BizProcess.Forms;
+
+namespace Zeta.Extreme.MongoDB.Integration {
+    class MongoDBAttachmentsSource : IAttachmentStorage {
+        // connection constants
+        const string DEFAULT_DB_NAME = "db";
+        const string DEFAULT_AV_COLLECTION_NAME = "AttachmentView";
+
+        // Connection information
+        private MongoClient client;
+        private MongoServer server;
+        private MongoDatabase database;
+        private MongoGridFS gridFS;
+        private MongoCollection mongoDBCurrentCollection;
+
+        private BsonDocument _currentDocument;
+        private string _currentDocumentID;
+
+        public MongoDBAttachmentsSource() {
+            this.MongoDBConnect();
+            this._currentDocument = new BsonDocument();
+        }
+
+        public IEnumerable<Attachment> Find(Attachment query) {
+            return null;
+        }
+
+        public void Save(Attachment attachment) {
+            this.HandleVariables(attachment);
+            this.AttachmentViewSave();
+
+            this._currentDocumentID = this._currentDocument["_id"].ToString();
+        }
+
+        public void Delete(Attachment attachment) {
+
+        }
+
+        /// <summary>
+        /// Open a stream to the current attachment
+        /// </summary>
+        /// <param name="attachment"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public Stream Open(Attachment attachment, FileAccess mode) {
+            return this.CreateStreamToCurrentFile(mode);
+        }
+
+        /// <summary>
+        /// Creates a stream to the file identified by _id in this._currentDocumentID
+        /// </summary>
+        /// <param name="mode">Acces mode to the stream</param>
+        /// <returns></returns>
+        private Stream CreateStreamToCurrentFile(FileAccess mode) {
+            return new MongoGridFSStream(
+                new MongoGridFSFileInfo(
+                    this.gridFS,
+                    this._currentDocumentID
+                ),
+                FileMode.Append,
+                mode
+            );
+        }
+
+        /// <summary>
+        /// Create a connection to database and select the collection in this.mongoDBCurrentCollection
+        /// </summary>
+        private void MongoDBConnect() {
+            this.client = new MongoClient();
+            this.server = client.GetServer();
+            this.database = server.GetDatabase(DEFAULT_DB_NAME);
+            this.gridFS = new MongoGridFS(this.database);
+            this.mongoDBCurrentCollection = this.database.GetCollection(DEFAULT_AV_COLLECTION_NAME);
+        }
+
+        /// <summary>
+        /// Save the attachment view information to database
+        /// </summary>
+        private void AttachmentViewSave() {
+            this.mongoDBCurrentCollection.Insert(this._currentDocument);
+        }
+
+        private void HandleVariables(Attachment attachment) {
+            this._currentDocument["extension"] = attachment.Extension ?? "";
+            this._currentDocument["Code"] = attachment.Uid ?? "";
+            this._currentDocument["type"] = attachment.Type ?? "";
+            this._currentDocument["Comment"] = attachment.Comment ?? "";
+        //    this._currentDocument["User"] = attachment.User ?? Application.Current.Principal.CurrentUser.Identity.Name;
+            this._currentDocument["MimeType"] = attachment.MimeType ?? "unknown/bin";
+
+          /*  this._currentDocument["tag"] = TagHelper.ToString(
+                attachment.Metadata.ToDictionary(
+                    _ => _.Key == "template" ? "form" : _.Key,
+                    _ => _.Value.ToString()
+                )
+            );
+
+            this._currentDocument["tag"] = TagHelper.Merge(attachment.Type, "/doctype:" + attachment.Type + "/");
+           */
+        }
+    }
+}
