6c4aa5eec54a73f72433940898ef008d8c0b2acf
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-03-17 09:41:27 +0600

    header fixed

--- Zeta.Extreme.Model/PocoClasses/ObjectGroup.cs
+++ Zeta.Extreme.Model/PocoClasses/ObjectGroup.cs
@@ -1,13 +1,21 @@
 #region LICENSE
-
-// Copyright 2012-2013 Media Technology LTD 
-// Original file : ObjectGroup.cs
-// Project: Zeta.Extreme.Poco
-// This code cannot be used without agreement from 
-// Media Technology LTD 
-
+// Copyright 2007-2013 Qorpent Team - http://github.com/Qorpent
+// Supported by Media Technology LTD 
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// PROJECT ORIGIN: Zeta.Extreme.Model/ObjectGroup.cs
 #endregion
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,21 +24,17 @@ using Zeta.Extreme.Model.SqlSupport;
 
 namespace Zeta.Extreme.Model {
 	public partial class ObjectGroup : IZetaObjectGroup {
-		 public virtual Guid Uid { get; set; }
+		public virtual string Tag { get; set; }
 
-		 public virtual string Tag { get; set; }
+		public virtual int Id { get; set; }
 
-		// 
+		public virtual string Name { get; set; }
 
-		 public virtual int Id { get; set; }
+		public virtual string Code { get; set; }
 
-		 public virtual string Name { get; set; }
+		public virtual string Comment { get; set; }
 
-		 public virtual string Code { get; set; }
-
-		 public virtual string Comment { get; set; }
-
-		 public virtual DateTime Version { get; set; }
+		public virtual DateTime Version { get; set; }
 
 		public virtual IList<IZetaMainObject> MainObjects {
 			get {
