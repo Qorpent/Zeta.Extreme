c812fd8bd11a41134585a6dac491a036660e6201
remalloc <remalloc@gmail.com>
2013-03-27 13:33:51 +0600

    Correct saving with seperating collections

--- Zeta.Extreme.MongoDB.Integration/MongoDbLogsSerializer.cs
+++ Zeta.Extreme.MongoDB.Integration/MongoDbLogsSerializer.cs
@@ -1,11 +1,32 @@
-﻿using Qorpent.Log;
+﻿using System;
+using Qorpent.Dsl;
+using Qorpent.Log;
 using MongoDB.Bson;
+using Qorpent.Mvc;
 
 namespace Zeta.Extreme.MongoDB.Integration {
-    class MongoDbLogsSerializer {
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MongoDbLogsSerializer {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public static BsonDocument LogMessageToBson(LogMessage message) {
             var document = new BsonDocument();
-
+            LogMessageToBson(message, document);
+            
+            return document;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="document"></param>
+        public static void LogMessageToBson(LogMessage message, BsonDocument document) {
             if(!string.IsNullOrEmpty(message.Name)) document.Set("name", message.Name);
             
             document.Set("level", message.Level);
@@ -21,10 +42,44 @@ namespace Zeta.Extreme.MongoDB.Integration {
             document.Set("time", message.Time);
             document.Set("lexInfo", message.LexInfo.ToString());
             
-            if (null != message.MvcCallInfo) document.Set("mvcCallInfo", message.MvcCallInfo.ToString());
-            if (null != message.MvcContext) document.Set("mvcContext", message.MvcContext.ToString());
+            if (null != message.MvcCallInfo) document.Set("mvcCallInfo", message.MvcCallInfo.ToBson());
+            if (null != message.MvcContext) document.Set("mvcContext", message.MvcContext.ToBson());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        public static LogMessage BsonToLogMessage(BsonDocument document) {
+            var message = new LogMessage();
+
+            BsonToLogMessage(document, message);
+
+            return message;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="message"></param>
+        public static void BsonToLogMessage(BsonDocument document, LogMessage message) {
+            BsonValue value;
             
-            return document;
+            if (document.TryGetValue("name", out value)) message.Name = value.ToString();
+            if (document.TryGetValue("level", out value)) message.Level = (LogLevel)value.ToInt32();
+            if (document.TryGetValue("code", out value)) message.Code = value.ToString();
+            if (document.TryGetValue("message", out value)) message.Message = value.ToString();
+            if (document.TryGetValue("hostObject", out value)) message.HostObject = value.ToString();
+            if (document.TryGetValue("user", out value)) message.User = value.ToString();
+            if (document.TryGetValue("error", out value)) message.Error = new Exception(value.ToString());
+            if (document.TryGetValue("server", out value)) message.Server = value.ToString();
+            if (document.TryGetValue("applicationName", out value)) message.ApplicationName = value.ToString();
+            if (document.TryGetValue("time", out value)) message.Time = value.ToUniversalTime();
+            //if (document.TryGetValue("lexInfo", out value)) message.LexInfo = value.ToString();
+            //if (document.TryGetValue("mvcCallInfo", out value)) message.MvcCallInfo = value.ToString();
+            //if (document.TryGetValue("mvcContext", out value)) message.MvcContext = value.ToString();
         }
     }
 }
