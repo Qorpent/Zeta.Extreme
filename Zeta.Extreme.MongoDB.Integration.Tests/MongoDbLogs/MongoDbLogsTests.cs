45f377882b7e185134acfe6dcb14a7af69bcfca7
Kirill Shirshov <remalloc@gmail.com>
2013-03-31 16:34:37 +0600

    CanWriteLogsWithPartitioning test added

--- Zeta.Extreme.MongoDB.Integration.Tests/MongoDbLogs/MongoDbLogsTests.cs
+++ Zeta.Extreme.MongoDB.Integration.Tests/MongoDbLogs/MongoDbLogsTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using MongoDB.Driver;
+using NUnit.Framework;
 
 namespace Zeta.Extreme.MongoDB.Integration.Tests {
     class MongoDbLogsTests : MongoDbLogsTestsBase {
@@ -16,10 +17,30 @@ namespace Zeta.Extreme.MongoDB.Integration.Tests {
         }
 
         [Test]
-        public void CanWriteLog() {
+        public void CanWriteLogsWithPartitioning() {
             var logMessageOrig = GetNewLogInstance();
 
-            for (int i = 0; i < 27; i++ ) _mongoDbLogs.Write(logMessageOrig);
+            for (int i = 0; i < 27; i++) {
+                _mongoDbLogs.Write(logMessageOrig);
+            }
+
+            var mongoClient = new MongoClient(ConnectionString);
+            var mongoServer = mongoClient.GetServer();
+            var mongoDatabase = mongoServer.GetDatabase(DatabaseName, new MongoDatabaseSettings());
+            var collections = mongoDatabase.GetCollectionNames();
+
+            foreach (var collection in collections) {
+                if (collection.StartsWith(LogsCollectionName + ".")) {
+                    Assert.AreEqual(
+                        mongoDatabase.GetCollection(collection).Count(),
+                        (
+                            (27 / MongoDbLayoutSpecification.MONGODBLOGS_STAT_PARTITION_COUNT)
+                                +
+                                (((27 % MongoDbLayoutSpecification.MONGODBLOGS_STAT_PARTITION_COUNT) != 0) ? (1) : (0))
+                        )
+                    );
+                }
+            }
         }
     }
 }
