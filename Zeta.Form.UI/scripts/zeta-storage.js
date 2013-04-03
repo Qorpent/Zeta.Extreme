a97c459c9ab5bb586a85339851e9578a67585309
Igor Myachin <i.myachin@gmail.com>
2013-04-03 14:39:49 +0600

    Небольшие изменения в zeta.storage. Хранилище вынесено на опции.

--- Zeta.Form.UI/scripts/zeta-storage.js
+++ Zeta.Form.UI/scripts/zeta-storage.js
@@ -1,56 +1,61 @@
 var root = window.zeta = window.zeta || {};
 
 !function($) {
-    var options = window.zeta.options;
-
-    var Storage = function() {
-
+    var Storage = function(opts) {
+        this.init();
+        if(!$.isEmptyObject(opts)) {
+            $.extend(this.options, opts);
+        }
     };
 
     Storage.prototype.init = function() {
-
+        this.options = this.options || {
+            storage : window.sessionStorage
+        };
     };
 
     Storage.prototype.support = function() {
         try {
-            return 'localStorage' in window && window['localStorage'] !== null;
+            return this.options.storage !== null;
         } catch (e) {
             return false;
         }
     };
 
-    Storage.prototype.getUsers = function() {
+    Storage.prototype.tryGetItem = function(item) {
+        var result = {};
         if (this.support()) {
-            return JSON.parse(window.localStorage.getItem("ZetaUsers")) || {};
-        }
-        return null;
-    };
-
-    Storage.prototype.getUser = function(login) {
-        if (this.support()) {
-            var users = this.getUsers();
-            if (!!users[login.toLowerCase()]) {
-                return options.asUserInfo(users[login.toLowerCase()]);
+            try {
+                result = JSON.parse(this.options.storage.getItem(item));
+            } catch(e) {
+                this.options.storage.removeItem(item);
+                return result;
             }
         }
         return null;
     };
 
+    Storage.prototype.getUsers = function() {
+        return this.tryGetItem("ZetaUsers");
+    };
+
+    Storage.prototype.getUser = function(login) {
+        var users = this.getUsers();
+        if (!!users[login.toLowerCase()]) {
+            return users[login.toLowerCase()];
+        }
+    };
+
     Storage.prototype.addUser = function(user) {
         if (this.support()) {
             var users = this.getUsers();
-            users[user.getLogin().toLowerCase()] = users[user.getLogin().toLowerCase()] || {};
-            $.extend(users[user.getLogin().toLowerCase()], user);
-            window.localStorage.setItem("ZetaUsers", JSON.stringify(users));
+            users[user.Login.toLowerCase()] = users[user.Login.toLowerCase()] || {};
+            $.extend(users[user.Login.toLowerCase()], user);
+            this.options.storage.setItem("ZetaUsers", JSON.stringify(users));
             users = null;
         }
     };
 
-    Storage.prototype.clear = function() {
-        if (this.support()) {
-            window.localStorage.setItem("ZetaUsers", {});
-        }
-    };
-
-    root.storage = new Storage();
+    root.localstorage = new Storage({ storage: window.localStorage });
+    root.sessionstorage = new Storage();
 }(window.jQuery);
