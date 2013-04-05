8b415de94ac39d3ada2246e1f9c912b21d054b91
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-04-05 10:50:54 +0600

    #ZC-493 tests written, still cannot reproduce

---
+++ Zeta.Extreme.Core.Tests/FeatureAndBugFix/Zc493AltDivTest.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using Zeta.Extreme.Core.Tests.CoreTests;
+
+namespace Zeta.Extreme.Core.Tests {
+	/// <summary>
+	/// </summary>
+	[TestFixture]
+	public class Zc493AltDivTest:SessionTestBase {
+		[Test]
+		[Timeout(5000)]
+		public void ZC493m231910() {
+			var q = Eval( new Query("m231910", "ZDk", 449, 2012, 112));
+			Assert.True(q.Result.IsComplete);
+			Assert.Null(q.Result.Error);
+		}
+
+		[Test]
+		[Timeout(5000)]
+		public void ZC493m230790()
+		{
+			var q = Eval(new Query("m230790", "ZDk", 449, 2012, 112));
+			Assert.True(q.Result.IsComplete);
+			Assert.Null(q.Result.Error);
+		}
+
+
+		[Test]
+		[Timeout(5000)]
+		public void ZC493m230750()
+		{
+			var q = Eval(new Query("m230750", "ZDk", 449, 2012, 112));
+			Assert.True(q.Result.IsComplete);
+			Assert.Null(q.Result.Error);
+		}
+
+		[Test]
+		[Timeout(5000)]
+		public void ZC493m230795()
+		{
+			var q = Eval(new Query("m230795", "ZDk", 449, 2012, 112));
+			Assert.True(q.Result.IsComplete);
+			Assert.Null(q.Result.Error);
+		}
+	}
+}
