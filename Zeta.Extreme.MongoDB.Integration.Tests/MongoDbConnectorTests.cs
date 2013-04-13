3b013bd5ba47e8bf5723174348047594ea0da704
Kirill Shirshov <remalloc@gmail.com>
2013-04-13 15:47:35 +0600

    Test for MongoDbConnector added

---
+++ Zeta.Extreme.MongoDB.Integration.Tests/MongoDbConnectorTests.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using NUnit.Framework;
+using MongoDB.Driver;
+
+namespace Zeta.Extreme.MongoDB.Integration.Tests {
+    class MongoDbConnectorTests : MongoDbConnectorTestsBase {
+        [Test]
+        public void CanConnectToMongoDb() {
+            // ensure that classes was created correct, but server still disconnected
+            Assert.AreEqual(MongoServerState.Disconnected, Connector.Server.State);
+
+            // do something (RW operation) to (real) connect to the server
+            Connector.Collection.Save(
+                new BsonDocument {
+                    {"testOk", true}
+                }
+            );
+
+            Assert.AreEqual(MongoServerState.Connected, Connector.Server.State);
+        }
+    }
+}
---
+++ Zeta.Extreme.MongoDB.Integration.Tests/MongoDbConnectorTestsBase.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Zeta.Extreme.MongoDB.Integration.Tests {
+    class MongoDbConnectorTestsBase {
+        protected const string DEFAULT_DB_NAME = "MongoDbConnectorTest";
+
+        protected MongoDbConnector Connector;
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp() {
+            Connector = new MongoDbConnector();
+        }
+    }
+}
