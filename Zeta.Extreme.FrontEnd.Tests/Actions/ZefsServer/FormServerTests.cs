f2b3da6d52c04e19437225b3bebef5c4d54ca218
remalloc <remalloc@gmail.com>
2013-04-15 17:07:26 +0600

    Correct adding IPrincipalSource to Application.Container in CanStartSession

---
+++ Zeta.Extreme.FrontEnd.Tests/Actions/ZefsServer/FormServerTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Qorpent;
+using Qorpent.Applications;
+using Qorpent.Events;
+using Qorpent.IO;
+using Qorpent.IoC;
+using Qorpent.Utils.Extensions;
+using Zeta.Extreme.BizProcess.Forms;
+using Zeta.Extreme.BizProcess.Themas;
+using Zeta.Extreme.Form.InputTemplates;
+using Zeta.Extreme.Form.SaveSupport;
+using Zeta.Extreme.Form.Themas;
+using Zeta.Extreme.FrontEnd.Actions;
+using Zeta.Extreme.Model;
+using Zeta.Extreme.Model.Inerfaces;
+using Zeta.Extreme.Model.MetaCaches;
+using Zeta.Extreme.Model.Querying;
+using Zeta.Extreme.Model.SqlSupport;
+
+using Zeta.Extreme.MongoDB.Integration;
+using Zeta.Extreme.Form;
+namespace Zeta.Extreme.FrontEnd.Tests {
+    class FormServerTests : ServiceBase {
+        private MongoDbAttachmentSource obj;
+        private Container _container;
+        private ComponentDefinition<IAttachmentStorage, MongoDbAttachmentSource> component;
+        private IApplication a;
+
+        [Test]
+        public void CanCreateFormServer() {
+            _container = new Container();
+            var component = _container.EmptyComponent();
+            component.Name = "attachment.source";
+            component.ServiceType = typeof(IAttachmentStorage);
+            component.ImplementationType = typeof(MongoDbAttachmentSource);
+            component.Lifestyle = Lifestyle.Transient;
+            component.Parameters["ConnectionString"] = "testconnection";
+            component.Parameters["Database"] = "testdatabase";
+            component.Parameters["Collection"] = "testcollection";
+            _container.Register(component);
+
+            a = new Application { Container = _container };
+            var t = a.Files.Resolve("~/.tmp/dgfdg.dll");
+            Assert.IsNotNull(t);
+
+        }
+    }
+}
---
+++ Zeta.Extreme.FrontEnd.Tests/FormServerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Principal;
+using NUnit.Framework;
+using Qorpent.Applications;
+using Qorpent.IoC;
+using Qorpent.Security;
+using Zeta.Extreme.BizProcess.Forms;
+using Zeta.Extreme.Form.InputTemplates;
+using Zeta.Extreme.Model;
+using Qorpent;
+
+namespace Zeta.Extreme.FrontEnd.Tests {
+    class FormServerTests : ServiceBase {
+        private FormServer _formServer;
+
+        [SetUp]
+        public void SetUp() {
+            _formServer = new FormServer();
+        }
+
+        [Test]
+        public void CanStartSession() {
+            var user = new GenericPrincipal(
+                new GenericIdentity(
+                    Environment.UserName
+                ),
+                
+                new string[] {}
+            );
+
+            Application = new Application {
+                Container = new SimpleContainer()
+            };
+
+
+            var component = Application.Container.EmptyComponent();
+            component.ServiceType = typeof(IPrincipalSource);
+            component.ImplementationType = typeof (DefaultPrincipalSource);
+            component.Lifestyle = Lifestyle.Transient;
+            Application.Container.Register(component);
+
+
+            Assert.IsNotNull(Application);
+            Assert.IsNotNull(Application.Principal);
+            Assert.IsNotNull(Application.Principal.CurrentUser);
+            Assert.IsNotNull(Application.Principal.CurrentUser.Identity);
+            Assert.IsNotNull(Application.Principal.CurrentUser.Identity.Name);
+            
+            _formServer.SetApplication(Application);
+
+            var session = _formServer.Start(
+                new InputTemplate(),
+                new Obj(),
+                2012,
+                2
+            );
+
+            Assert.IsNotNull(session);
+        }
+    }
+}
