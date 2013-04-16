aeec04bcbbad1d06ee705e4bd97ecffd61d51d4d
remalloc <remalloc@gmail.com>
2013-04-16 15:58:23 +0600

    Several correct tests

--- Zeta.Extreme.FrontEnd.Tests/FormServerTests.cs
+++ Zeta.Extreme.FrontEnd.Tests/FormServerTests.cs
@@ -6,12 +6,22 @@ using Qorpent.IoC;
 using Qorpent.Security;
 using Zeta.Extreme.BizProcess.Forms;
 using Zeta.Extreme.Form.InputTemplates;
+using Zeta.Extreme.Form.Themas;
 using Zeta.Extreme.Model;
 using Qorpent;
 
 namespace Zeta.Extreme.FrontEnd.Tests {
     class FormServerTests : ServiceBase {
         private FormServer _formServer;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class FormRowProvider : IFormRowProvider {
+            public FormStructureRow[] GetRows(IFormSession session) {
+                throw new NotImplementedException();
+            }
+        }
 
         [SetUp]
         public void SetUp() {
@@ -20,24 +30,33 @@ namespace Zeta.Extreme.FrontEnd.Tests {
 
         [Test]
         public void CanStartSession() {
-            var user = new GenericPrincipal(
-                new GenericIdentity(
-                    Environment.UserName
-                ),
-                
-                new string[] {}
-            );
-
             Application = new Application {
                 Container = new SimpleContainer()
             };
 
+            var comp1 = Application.Container.EmptyComponent();
+            comp1.ServiceType = typeof(IPrincipalSource);
+            comp1.ImplementationType = typeof (DefaultPrincipalSource);
+            comp1.Lifestyle = Lifestyle.Transient;
+            Application.Container.Register(comp1);
 
-            var component = Application.Container.EmptyComponent();
-            component.ServiceType = typeof(IPrincipalSource);
-            component.ImplementationType = typeof (DefaultPrincipalSource);
-            component.Lifestyle = Lifestyle.Transient;
-            Application.Container.Register(component);
+            var comp2 = Application.Container.EmptyComponent();
+            comp2.Name = "SampleCode.row.preparator";
+            comp2.ServiceType = typeof (IFormRowProvider);
+            comp2.ImplementationType = typeof (FormRowProvider);
+            comp2.Lifestyle = Lifestyle.Transient;
+            Application.Container.Register(comp2);
+
+            var obj = new Obj();
+            var template = new InputTemplate {
+                Thema = new Thema {
+                    Code = "SampleCode"
+                }
+            };
+
+            var sess = new FormSession(template, 2012, 2, obj);
+
+            Assert.IsNotNull(sess);
 
 
             Assert.IsNotNull(Application);
@@ -47,15 +66,63 @@ namespace Zeta.Extreme.FrontEnd.Tests {
             Assert.IsNotNull(Application.Principal.CurrentUser.Identity.Name);
             
             _formServer.SetApplication(Application);
+        }
 
-            var session = _formServer.Start(
-                new InputTemplate(),
-                new Obj(),
-                2012,
-                2
-            );
+        [Test]
+        public void CanCreateFormServerInstance() {
+            var formServer = new FormServer();
+            Assert.IsNotNull(formServer);
+        }
 
-            Assert.IsNotNull(session);
+        [Test]
+        public void CanSetApplicationToAFormServerInstance() {
+            var formServer = new FormServer();
+            var application = new Application {
+                Container = new SimpleContainer()
+            };
+
+            Assert.IsNotNull(formServer);
+            Assert.IsNotNull(application);
+
+            formServer.SetApplication(application);
+        }
+
+        [Test]
+        public void CanWorkWithContainer() {
+            var formServer = new FormServer();
+            var application = new Application {
+                Container = new SimpleContainer()
+            };
+
+            Assert.IsNotNull(formServer);
+            Assert.IsNotNull(application);
+
+            var comp1 = Application.Container.EmptyComponent();
+            comp1.ServiceType = typeof(IPrincipalSource);
+            comp1.ImplementationType = typeof(DefaultPrincipalSource);
+            comp1.Lifestyle = Lifestyle.Transient;
+            Application.Container.Register(comp1);
+
+            var comp2 = Application.Container.EmptyComponent();
+            comp2.Name = "SampleCode.row.preparator";
+            comp2.ServiceType = typeof(IFormRowProvider);
+            comp2.ImplementationType = typeof(FormRowProvider);
+            comp2.Lifestyle = Lifestyle.Transient;
+            Application.Container.Register(comp2);
+
+            var obj = new Obj();
+            var template = new InputTemplate {
+                Thema = new Thema {
+                    Code = "SampleCode"
+                }
+            };
+
+            var sess = new FormSession(template, 2012, 2, obj);
+
+            Assert.IsNotNull(sess);
+            Assert.IsNotNull(Application);
+
+            formServer.SetApplication(application);
         }
     }
 }
