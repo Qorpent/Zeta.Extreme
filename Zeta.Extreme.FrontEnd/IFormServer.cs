37a12ae88b3453857b60243798d06cf558838560
remalloc <remalloc@gmail.com>
2013-04-16 17:31:50 +0600

    IFormServer aded

---
+++ Zeta.Extreme.FrontEnd/IFormServer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Security.Principal;
+using Qorpent.Applications;
+using Zeta.Extreme.BizProcess.Themas;
+using Zeta.Extreme.Form.SaveSupport;
+using Zeta.Extreme.Model.Inerfaces;
+
+namespace Zeta.Extreme.FrontEnd {
+    /// <summary>
+    ///     FormServer Interface
+    /// </summary>
+    public interface IFormServer {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        string GetCommonETag();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        DateTime GetCommonLastModified();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        string GetUserETag(IPrincipal user = null);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="application"></param>
+        void Execute(IApplication application);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IFormSessionDataSaver GetSaver();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        object GetServerStateInfo();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="obj"></param>
+        /// <param name="year"></param>
+        /// <param name="period"></param>
+        /// <param name="initsavemode"></param>
+        /// <returns></returns>
+        FormSession Start(
+            IInputTemplate template,
+            IZetaMainObject obj,
+            int year,
+            int period,
+            bool initsavemode = false
+        );
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Reload();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void CheckGlobalReload();
+    }
+}
