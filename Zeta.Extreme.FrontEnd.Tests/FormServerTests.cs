7ab5a61130265a493f9a5f053e7911ac80b8b231
remalloc <remalloc@gmail.com>
2013-04-16 16:00:37 +0600

    Test updated

--- Zeta.Extreme.FrontEnd.Tests/FormServerTests.cs
+++ Zeta.Extreme.FrontEnd.Tests/FormServerTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Principal;
 using NUnit.Framework;
 using Qorpent.Applications;
 using Qorpent.IoC;
@@ -15,57 +14,12 @@ namespace Zeta.Extreme.FrontEnd.Tests {
         private FormServer _formServer;
 
         /// <summary>
-        /// 
+        ///     Перектырый класс для тестовой инициализации
         /// </summary>
         public class FormRowProvider : IFormRowProvider {
             public FormStructureRow[] GetRows(IFormSession session) {
                 throw new NotImplementedException();
             }
-        }
-
-        [SetUp]
-        public void SetUp() {
-            _formServer = new FormServer();
-        }
-
-        [Test]
-        public void CanStartSession() {
-            Application = new Application {
-                Container = new SimpleContainer()
-            };
-
-            var comp1 = Application.Container.EmptyComponent();
-            comp1.ServiceType = typeof(IPrincipalSource);
-            comp1.ImplementationType = typeof (DefaultPrincipalSource);
-            comp1.Lifestyle = Lifestyle.Transient;
-            Application.Container.Register(comp1);
-
-            var comp2 = Application.Container.EmptyComponent();
-            comp2.Name = "SampleCode.row.preparator";
-            comp2.ServiceType = typeof (IFormRowProvider);
-            comp2.ImplementationType = typeof (FormRowProvider);
-            comp2.Lifestyle = Lifestyle.Transient;
-            Application.Container.Register(comp2);
-
-            var obj = new Obj();
-            var template = new InputTemplate {
-                Thema = new Thema {
-                    Code = "SampleCode"
-                }
-            };
-
-            var sess = new FormSession(template, 2012, 2, obj);
-
-            Assert.IsNotNull(sess);
-
-
-            Assert.IsNotNull(Application);
-            Assert.IsNotNull(Application.Principal);
-            Assert.IsNotNull(Application.Principal.CurrentUser);
-            Assert.IsNotNull(Application.Principal.CurrentUser.Identity);
-            Assert.IsNotNull(Application.Principal.CurrentUser.Identity.Name);
-            
-            _formServer.SetApplication(Application);
         }
 
         [Test]
@@ -97,18 +51,18 @@ namespace Zeta.Extreme.FrontEnd.Tests {
             Assert.IsNotNull(formServer);
             Assert.IsNotNull(application);
 
-            var comp1 = Application.Container.EmptyComponent();
+            var comp1 = application.Container.EmptyComponent();
             comp1.ServiceType = typeof(IPrincipalSource);
             comp1.ImplementationType = typeof(DefaultPrincipalSource);
             comp1.Lifestyle = Lifestyle.Transient;
-            Application.Container.Register(comp1);
+            application.Container.Register(comp1);
 
-            var comp2 = Application.Container.EmptyComponent();
+            var comp2 = application.Container.EmptyComponent();
             comp2.Name = "SampleCode.row.preparator";
             comp2.ServiceType = typeof(IFormRowProvider);
             comp2.ImplementationType = typeof(FormRowProvider);
             comp2.Lifestyle = Lifestyle.Transient;
-            Application.Container.Register(comp2);
+            application.Container.Register(comp2);
 
             var obj = new Obj();
             var template = new InputTemplate {
@@ -120,7 +74,7 @@ namespace Zeta.Extreme.FrontEnd.Tests {
             var sess = new FormSession(template, 2012, 2, obj);
 
             Assert.IsNotNull(sess);
-            Assert.IsNotNull(Application);
+            Assert.IsNotNull(application);
 
             formServer.SetApplication(application);
         }
