f36aad0d32664c70a6adfeeb2441094f01b7dede
remalloc <remalloc@gmail.com>
2013-04-25 11:09:21 +0600

    GetClusterNodeLoad moved to using builder

--- Zeta.Extreme.FrontEnd/Actions/ZefsServer/GetClusterNodeLoad.cs
+++ Zeta.Extreme.FrontEnd/Actions/ZefsServer/GetClusterNodeLoad.cs
@@ -14,24 +14,7 @@ namespace Zeta.Extreme.FrontEnd.Actions.ZefsServer {
         /// </summary>
         /// <returns></returns>
         protected override object MainProcess() {
-            Int64 load = 0;
-            
-            load += (FormServersState.CurrentReloadOperations)*(5);
-            load += (FormSessionsState.CurrentFormLoadingOperations)*(10);
-            load += (FormSessionsState.CurrentFormRenderingOperations)*(3);
-            load += (FormSessionsState.LockFormCurrentOperations)*(1);
-            load += (FormSessionsState.CurrentAttachFileOperations)*(2);
-
-            var loadInfo = new Dictionary<string, Int64> {
-                {"CurrentReloadOperations", (FormServersState.CurrentReloadOperations)*(5)},
-                {"CurrentFormLoadingOperations", (FormSessionsState.CurrentFormLoadingOperations)*(10)},
-                {"CurrentFormRenderingOperations", (FormSessionsState.CurrentFormRenderingOperations)*(3)},
-                {"LockFormCurrentOperations", (FormSessionsState.LockFormCurrentOperations)*(1)},
-                {"CurrentAttachFileOperations", (FormSessionsState.CurrentAttachFileOperations)*(2)},
-                {"Summary", load}
-            };
-
-            return loadInfo;
+            return ClusterNodeLoadBuilder.Build();
         }
     }
 }
