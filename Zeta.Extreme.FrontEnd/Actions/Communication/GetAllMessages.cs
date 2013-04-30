cde285cd5a3668077a0b4ac96f8de0d0892761a5
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-04-30 15:18:05 +0600

    поддержка роли саппорта

--- Zeta.Extreme.FrontEnd/Actions/Communication/GetAllMessages.cs
+++ Zeta.Extreme.FrontEnd/Actions/Communication/GetAllMessages.cs
@@ -35,33 +35,48 @@ namespace Zeta.Extreme.FrontEnd.Actions.Communication {
 		protected override object MainProcess() {
 			var lastread = _provider.GetLastRead(Context.User.Identity.Name);
 			var items = new FormChatItem[]{};
-			if (IsInRole("BUDGET"))
-			{
+			if (IsInRole("BUDGET")) {
 				var myobjs = GetMyOwnObjects();
 				if (0 != myobjs.Length) {
 					items = _provider.FindAll(Context.User.Identity.Name, From, myobjs,
-					                          new[] { "objcurrator"},
+					                          new[] {"objcurrator"},
 					                          includeArchived: ShowArchived).OrderByDescending(_ => _.Time).ToArray();
 				}
-				var curforms = ((ExtremeFormProvider) FormServer.Default.FormProvider).Factory.GetAll().Where(
-					_ => _.GetParameter("hold.responsibility", "") == Context.User.Identity.Name.ToLower()
-					).SelectMany(_ => new[] {_.Code + "A.in", _.Code + "B.in"}).ToArray();
 
-				if (curforms.Length != 0) {
+				var curforms = ((ExtremeFormProvider)FormServer.Default.FormProvider).Factory.GetAll().Where(
+					_ => _.GetParameter("hold.responsibility", "") == Context.User.Identity.Name.ToLower()
+					).SelectMany(_ => new[] { _.Code + "A.in", _.Code + "B.in" }).ToArray();
+
+				if (curforms.Length != 0)
+				{
 					var curformitems = _provider.FindAll(Context.User.Identity.Name, From, null,
-					                                     new[] {"formcurrator"}, curforms.ToArray(), ShowArchived).OrderByDescending(_ => _.Time).ToArray();
-					if (curformitems.Any()) {
+														 new[] { "formcurrator" }, curforms.ToArray(), ShowArchived).OrderByDescending(_ => _.Time).ToArray();
+					if (curformitems.Any())
+					{
 						items = items.Union(curformitems).OrderByDescending(_ => _.Time).ToArray();
 					}
 				}
-
 			}
+
 			else if (!IsInRole("ADMIN") && !IsInRole("SYS_ALLOBJECTS"))
 			{
 				var myobjs = GetMyAccesibleObjects();
 				var myforms = GetMyFormCodes();
 				items = _provider.FindAll(Context.User.Identity.Name, From, myobjs, forms: myforms, includeArchived:ShowArchived).OrderByDescending(_=>_.Time).ToArray();
 			}
+
+			
+
+			if (IsInRole("SUPPORT", true)) {
+				var supportitems = _provider.FindAll(Context.User.Identity.Name, From, null,
+													 new[] { "support" }, null, ShowArchived).OrderByDescending(_ => _.Time).ToArray();
+				if (supportitems.Any())
+				{
+					items = items.Union(supportitems).OrderByDescending(_ => _.Time).ToArray();
+				}
+				
+			}
+
 			foreach (var chatitem in items) {
 				if (chatitem.Time > lastread) {
 					chatitem.Userdata["isnew"] = true;
