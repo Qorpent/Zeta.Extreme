5b109ef25c2ab00d36fa7d7d0e3f52a5cc41241b
remalloc <remalloc@gmail.com>
2013-04-30 16:23:17 +0600

    Moved to using IoC

---
+++ Zeta.Extreme.FrontEnd/NodeLoadingPoints.cs
@@ -0,0 +1,128 @@
+﻿namespace Zeta.Extreme.FrontEnd {
+    /// <summary>
+    ///     Класс, хранящий значения вес каждого из элементов рассчёта
+    /// </summary>
+    public class NodeLoadingPoints : INodeLoadingPoints {
+        private int _totalQueriesHandled = 1;
+        private int _totalCpuMinute = 1;
+        private int _totalServerReloads = 20;
+        private int _totalSessionsHandled = 10;
+
+        private int _currentReloadOperations = 5;
+        private int _currentFormLoadingOperations = 10;
+        private int _currentFormRenderingOperations = 3;
+        private int _currentLockFormOperations = 1;
+        private int _currentAttachFileOperations = 2;
+        private int _currentDataSaveOperations = 7;
+
+        private int _a = 1;
+        private int _p = 2;
+        private int _l = 3;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int TotalQueriesHandled {
+            get { return _totalQueriesHandled; }
+            set { _totalQueriesHandled = value; }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public int TotalCpuMinute {
+            get { return _totalCpuMinute; }
+            set { _totalCpuMinute = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int TotalServerReloads {
+            get { return _totalServerReloads; }
+            set { _totalServerReloads = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int TotalSessionsHandled {
+            get { return _totalSessionsHandled; }
+            set { _totalSessionsHandled = value; }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CurrentReloadOperations {
+            get { return _currentReloadOperations; }
+            set { _currentReloadOperations = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CurrentFormLoadingOperations {
+            get { return _currentFormLoadingOperations; }
+            set { _currentFormLoadingOperations = value; }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CurrentFormRenderingOperations {
+            get { return _currentFormRenderingOperations; }
+            set { _currentFormRenderingOperations = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CurrentLockFormOperations {
+            get { return _currentLockFormOperations; }
+            set { _currentLockFormOperations = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CurrentAttachFileOperations {
+            get { return _currentAttachFileOperations; }
+            set { _currentAttachFileOperations = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CurrentDataSaveOperations {
+            get { return _currentDataSaveOperations; }
+            set { _currentDataSaveOperations = value; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int A {
+            get { return _a; }
+            set { _a = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int P {
+            get { return _p; }
+            set { _p = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int L {
+            get { return _l; }
+            set { _l = value; }
+        }
+    }
+}
--- Zeta.Extreme.FrontEnd/NodeLoadingStatistics.cs
+++ Zeta.Extreme.FrontEnd/NodeLoadingStatistics.cs
@@ -3,49 +3,43 @@ using Qorpent;
 
 namespace Zeta.Extreme.FrontEnd {
     class NodeLoadingStatistics {
-        internal class Points {
-            public const int TotalQueriesHandled = 1;
-            public const int TotalCpuMinute = 1;
-            public const int TotalServerReloads = 20;
-            public const int TotalSessionsHandled = 10;
+        INodeLoadingPoints LoadingPoints { get; set; }
 
-            public const int CurrentReloadOperations = 5;
-            public const int CurrentFormLoadingOperations = 10;
-            public const int CurrentFormRenderingOperations = 3;
-            public const int CurrentLockFormOperations = 1;
-            public const int CurrentAttachFileOperations = 2;
-            public const int CurrentDataSaveOperations = 7;
-
-            public const int A = 1;
-            public const int P = 2;
-            public const int L = 3;
+        public NodeLoadingStatistics(INodeLoadingPoints loadingPoints) {
+            if (loadingPoints != null) {
+                LoadingPoints = loadingPoints;
+            } else {
+                LoadingPoints = new NodeLoadingPoints();  
+            }
         }
 
-        public static int Age(int queriesDivider = 1) {
-            return (ServiceState.TotalQueriesHandled * Points.TotalQueriesHandled) / queriesDivider +
-                   Convert.ToInt32(ServiceState.CpuMinutes) * Points.TotalCpuMinute +
-                   FormServersState.TotalReloadsCount * Points.TotalServerReloads +
-                   FormServersState.TotalSessionsHandled * Points.TotalSessionsHandled;
+        public double Age(int queriesDivider = 1) {
+            return (ServiceState.TotalQueriesHandled / queriesDivider) * LoadingPoints.TotalQueriesHandled +
+                   Convert.ToInt32(ServiceState.CpuMinutes) * LoadingPoints.TotalCpuMinute +
+                   FormServersState.TotalReloadsCount * LoadingPoints.TotalServerReloads +
+                   FormServersState.TotalSessionsHandled * LoadingPoints.TotalSessionsHandled;
         }
 
-        public static int Load() {
-            return FormServersState.CurrentReloadOperations * Points.CurrentReloadOperations +
-                   FormSessionsState.CurrentFormLoadingOperations * Points.CurrentFormLoadingOperations +
-                   FormSessionsState.CurrentFormRenderingOperations * Points.CurrentFormRenderingOperations +
-                   FormSessionsState.CurrentLockFormOperations * Points.CurrentLockFormOperations +
-                   FormSessionsState.CurrentAttachFileOperations * Points.CurrentAttachFileOperations +
-                   FormSessionsState.CurrentDataSaveOperations * Points.CurrentDataSaveOperations;
+        public double Load() {
+            return FormServersState.CurrentReloadOperations * LoadingPoints.CurrentReloadOperations +
+                   FormSessionsState.CurrentFormLoadingOperations * LoadingPoints.CurrentFormLoadingOperations +
+                   FormSessionsState.CurrentFormRenderingOperations * LoadingPoints.CurrentFormRenderingOperations +
+                   FormSessionsState.CurrentLockFormOperations * LoadingPoints.CurrentLockFormOperations +
+                   FormSessionsState.CurrentAttachFileOperations * LoadingPoints.CurrentAttachFileOperations +
+                   FormSessionsState.CurrentDataSaveOperations * LoadingPoints.CurrentDataSaveOperations;
         }
-    
-        public static int Power() {
+
+        public double Power() {
             var estimatedCpuMinutes = ServiceState.CpuMinutes > 0 ? ServiceState.CpuMinutes : 1;
-            var estimatedTimeToLoadData = FormServersState.TotalTimeToLoadData.TotalSeconds > 0 ? Convert.ToInt32(FormServersState.TotalTimeToLoadData.TotalSeconds) : 1;
-            return (ServiceState.TotalQueriesHandled / estimatedCpuMinutes) +
-                   (FormServersState.TotalSessionsHandled / estimatedTimeToLoadData);
+            var estimatedTimeToLoadData = FormServersState.TotalTimeToLoadData.TotalSeconds > 0 ? FormServersState.TotalTimeToLoadData.TotalSeconds : 1;
+            return 1.0 / (
+                    Convert.ToDouble(ServiceState.TotalQueriesHandled / estimatedCpuMinutes) +
+                    Convert.ToDouble(FormServersState.TotalSessionsHandled / estimatedTimeToLoadData)
+                );
         }
 
-        public static int Availability() {
-            return Points.A * Age(100) + Points.P * Power() + Points.L * Load();
+        public double Availability() {
+            return 1 / (LoadingPoints.A * Age() + LoadingPoints.P * Power() + LoadingPoints.L * Load());
         }
     }
 }
