605c56e6045e0bc7c609a331ce391a8d5444daa4
Fagim Sadykov <fagim.sadykov@gmail.com>
2013-05-03 12:20:57 +0600

    Исправленный расчет числа сообщений

--- Zeta.Extreme.FrontEnd/Actions/Communication/GetUpdateCount.cs
+++ Zeta.Extreme.FrontEnd/Actions/Communication/GetUpdateCount.cs
@@ -10,7 +10,9 @@
 
 #endregion
 
+using System.Linq;
 using Qorpent.Mvc;
+using Zeta.Extreme.Form.Themas;
 
 namespace Zeta.Extreme.FrontEnd.Actions.Communication {
 	/// <summary>
@@ -25,17 +27,39 @@ namespace Zeta.Extreme.FrontEnd.Actions.Communication {
 		/// <returns>
 		/// </returns>
 		protected override object MainProcess() {
-			if (IsInRole("BUDGET")) {
+			//	var lastread = _provider.GetLastRead(Context.User.Identity.Name);
+			long count = 0;
+			if (IsInRole("BUDGET"))
+			{
 				var myobjs = GetMyOwnObjects();
-				if (0 == myobjs.Length) return 0;
-				return _provider.GetUpdatesCount(Context.User.Identity.Name, myobjs);
+				if (0 != myobjs.Length) {
+					count += _provider.GetUpdatesCount(Context.User.Identity.Name, myobjs,
+					                                   new[] {"objcurrator"});
+				}
+
+				var curforms = ((ExtremeFormProvider)FormServer.Default.FormProvider).Factory.GetAll().Where(
+					_ => _.GetParameter("hold.responsibility", "") == Context.User.Identity.Name.ToLower()
+					).SelectMany(_ => new[] { _.Code + "A.in", _.Code + "B.in" }).ToArray();
+
+				if (curforms.Length != 0) {
+					count += _provider.GetUpdatesCount(Context.User.Identity.Name, null, new[] {"formcurrator"}, curforms.ToArray());
+				}
 			}
-			if (!IsInRole("ADMIN") && !IsInRole("SYS_ALLOBJECTS")) {
+
+			else if (!IsInRole("ADMIN") && !IsInRole("SYS_ALLOBJECTS"))
+			{
 				var myobjs = GetMyAccesibleObjects();
 				var myforms = GetMyFormCodes();
-				return _provider.GetUpdatesCount(Context.User.Identity.Name,myobjs,forms:myforms);
+				count += _provider.GetUpdatesCount(Context.User.Identity.Name, myobjs, forms: myforms);
 			}
-			return 0;
+
+
+
+			if (IsInRole("SUPPORT", true)) {
+				count += _provider.GetUpdatesCount(Context.User.Identity.Name, null,   new[] {"support"}, null);
+			}
+
+			return count;
 		}
 	}
 }
