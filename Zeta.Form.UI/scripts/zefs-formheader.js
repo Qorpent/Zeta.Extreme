c58c26c6a9f99196b3b5c2b0ab95d7c40929894e
Igor Myachin <i.myachin@gmail.com>
2013-05-07 11:57:20 +0600

    В шапку добавлена кнопка отображающая список пользователей текущей формы

--- Zeta.Form.UI/scripts/zefs-formheader.js
+++ Zeta.Form.UI/scripts/zefs-formheader.js
@@ -11,7 +11,11 @@
         $(h.find('span').first()).text(zefs.getperiodbyid(s.Period));
         if (!!s.FormInfo.HoldResponsibility) {
             var u = $('<span class="label label-success" style="display: none;"/>').text(s.FormInfo.HoldResponsibility).css("margin-left", 3);
-            h.append($('<span class="label label-success kurator"/>').html('Куратор формы <i class="icon icon-white"/>').click(function() {u.trigger('click')}),u);
+            var k = $('<span class="label label-success kurator"/>').html('Куратор формы <i class="icon icon-white"/>');
+            k.click(function() {
+                u.trigger('click');
+            });
+            h.append(k);
             u.zetauser();
         }
         if (!!s.ObjInfo.Currency) {
@@ -38,6 +42,46 @@
             zefs.myform.currentSession.Year + " год"
         );
         InsertPeriod();
+    });
+    $(window.zefs).on(window.zefs.handlers.on_formusersload, function() {
+        if (!!zefs.myform.users) {
+            var b = $('<span class="label label-warning dropdown-toggle pull-right" data-toggle="dropdown"/>').text('Пользователи');
+            var header = $('<tr/>').append(
+                $('<th/>').text("Пользователь"),
+                $('<th/>').text("Должность"),
+                $('<th/>').text("Телефон")
+            );
+            if (zeta.user.getRealIsAdmin()) {
+                header.append($('<th/>').text(""));
+            }
+            var content = $('<table class="table-bordered table" />').append(
+                $('<thead/>').append(header)
+            );
+            var body = $('<tbody/>');
+            content.append(body);
+            $.each(zefs.myform.users, function(i, e) {
+                var u = $('<span class="label label-info"/>').text(e.Login);
+                var tr = $('<tr/>').append(
+                    $('<td/>').append(u),
+                    $('<td/>').text(e.Dolzh),
+                    $('<td/>').text(e.Contact)
+                );
+                if (zeta.user.getRealIsAdmin()) {
+                    var l = $('<button class="btn btn-mini"/>').text("Войти от");
+                    tr.append($('<td/>').append(l));
+                    l.click(function() {
+                        zeta.api.security.impersonateall({Target: e.Login});
+                    });
+                }
+                u.zetauser();
+                body.append(tr);
+            });
+            b.click(function() {
+                var params = { title: "Cписок пользователей данной формы", content: content, width: 800 };
+                $(window.zeta).trigger(window.zeta.handlers.on_modal, params);
+            });
+            h.append(b);
+        }
     });
     window.zefs.api.metadata.getperiods.onSuccess(function(e, result) {
         if($.isEmptyObject(window.zefs.periods)) {
