4d105b70ffbda8647da56e3aa92b580dfe29530a
Igor Myachin <i.myachin@gmail.com>
2013-05-08 14:31:00 +0600

    - В историю ячеек добавлено поле "Валюта"

--- Zeta.Form.UI/scripts/zefs.js
+++ Zeta.Form.UI/scripts/zefs.js
@@ -416,6 +416,9 @@ root.init = root.init ||
                 $('<tr/>').append($('<td/>').text("Строка"), $('<td/>').text("(" + result.cell.rowcode + ") " + result.cell.rowname)),
                 $('<tr/>').append($('<td/>').text("Период"), $('<td/>').text("(" + result.cell.period + ") " + window.zefs.getperiodbyid(result.cell.period)))
             ).hide();
+            if (!!result.cell.currency) {
+                cellinfo.append($('<tr/>').append($('<td/>').text("Валюта"), $('<td/>').text(result.cell.currency)));
+            }
             cellinfotoggle.click(function() { cellinfo.toggle() });
             var cellhistory = $('<table class="table table-bordered table-striped"/>');
             var insertvalue = function(value) {
