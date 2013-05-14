5a6d1b435cef40b7a628091bcff9b7ed287b90bb
Igor Myachin <i.myachin@gmail.com>
2013-05-14 09:55:13 +0600

    - После изменения статуса блокировки формы, соответствующий кураторский объект так же изменяет статус

--- Zeta.Form.UI/scripts/zefs-extrapannel.js
+++ Zeta.Form.UI/scripts/zefs-extrapannel.js
@@ -9,14 +9,32 @@
         location.hash = location.hash.replace(/obj=\d+/gi,"obj=" + id);
         location.reload();
     };
+    var ChangeState = function(element, state) {
+        element = $(element);
+        switch (state) {
+            case "0ISCHECKED" :
+                element.addClass("label-success");
+                break;
+            case "0ISOPEN" :
+                element.addClass("label-important");
+                break;
+            case "0ISBLOCK" :
+                element.addClass("label-warning");
+                break;
+        }
+    };
     $(window.zefs).on(window.zefs.handlers.on_objectsload, function() {
         if (!$.isEmptyObject(window.zefs.myobjs)) {
             $('.zefsformheader').first().css("top", 60);
             $('.zefsalerter').first().css("top", 64);
+            var currentObj = zefs.api.getParameters()["obj"] || -1;
             $.each(window.zefs.myobjs, function(i, obj) {
                 var name = obj.shortname || obj.name || "";
                 var l = $('<span class="label"/>');
                 l.attr("value", obj.id);
+                if (obj.id == currentObj) {
+                    l.addClass("currentobj");
+                }
                 var r = name.replace(/УГМК-?/, "").match(/"([^"]+)"/);
                 l.text(r != null ? r[1] : name);
                 l.click(function() { ChangeObject(obj.id) });
@@ -25,20 +43,16 @@
                     url:"zefs/getcurratorlockstate.json.qweb",
                     data : { session : zefs.myform.sessionId, objid: obj.id }
                 }).success(function(s) {
-                    switch (s) {
-                        case "0ISCHECKED" :
-                            l.addClass("label-success");
-                            break;
-                        case "0ISOPEN" :
-                            l.addClass("label-important");
-                            break;
-                        case "0ISBLOCK" :
-                            l.addClass("label-warning");
-                            break;
-                    }
+                    ChangeState(l, s);
                 });
             });
             extrapannel.body.show();
+        }
+    });
+    $(zefs).on(zefs.handlers.on_getlockload, function() {
+        var current = $("span.currentobj");
+        if (current.length > 0) {
+            ChangeState(current.first(), zefs.myform.lock.state);
         }
     });
     root.console.RegisterWidget(extrapannel);
