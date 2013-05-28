28a233918742dc055ccf91d202292456cc18993c
Kirill Shirshov <remalloc@gmail.com>
2013-05-28 22:54:53 +0600

    Margarita initial commit - class for control serversMap in hot state

---
+++ Zeta.Form.UI/scripts/margarita.js
@@ -0,0 +1,33 @@
+(function() {
+	var global = window.Margarita = this;
+	
+	this.select = {
+		server : {
+		
+		},
+		
+		app : {
+		
+		}
+	},
+	
+	this.remove = {
+		server : {
+		
+		},
+		
+		app : {
+		
+		}
+	},
+	
+	this.add = {
+		server : {
+		
+		},
+		
+		app : {
+		
+		}
+	};
+})(serversMap);
