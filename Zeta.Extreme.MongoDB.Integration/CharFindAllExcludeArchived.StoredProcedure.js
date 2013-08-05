47c0e64bef6e506a9261c4d9e48815fe37665280
Kirill Shirshov <remalloc@gmail.com>
2013-08-05 12:30:45 +0600

    More stable stored procedure

--- Zeta.Extreme.MongoDB.Integration/CharFindAllExcludeArchived.StoredProcedure.js
+++ Zeta.Extreme.MongoDB.Integration/CharFindAllExcludeArchived.StoredProcedure.js
@@ -4,7 +4,7 @@ function(collectionName, user, mainquery) {
 		function(x) {
 			h[x._id.valueOf()] = x;
 			db[collectionName + "_usr"].find(
-				{'user' : user, 'archive' : true, 'message_id' : x._id}
+				{'user' : user, 'archive' : true, 'message_id' : x._id.valueOf()}
 			).forEach(
 				function(z) {
 					delete h[z.message_id]
