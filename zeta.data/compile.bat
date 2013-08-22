ed9e6eac8a51375e5649b06b906c4fc51659ab50
comdiv <fagim.sadykov@gmail.com>
2013-08-22 09:08:16 +0600

    Уточненный compile.bat, исходники помещены в папку

--- zeta.data/compile.bat
+++ zeta.data/compile.bat
@@ -1,2 +1,4 @@
-..\..\.build\bin\all\bsc.exe
+..\..\.build\bin\all\bsc.exe --log .output/.log
+..\..\.build\bin\all\bsc.exe --log .output/.old/.log --out .output/.old --set-OLDONLY --set-USEALL
+..\..\.build\bin\all\bsc.exe --log .output/.all/.log --out .output/.all --set-USEALL
 pause
