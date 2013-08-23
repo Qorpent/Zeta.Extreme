4160f0f2206e59f8363e5303315b78a0664adc21
comdiv <fagim.sadykov@gmail.com>
2013-08-23 16:03:33 +0600

    compile.bat fixes

--- zeta.data/compile.bat
+++ zeta.data/compile.bat
@@ -1,4 +1,5 @@
-..\..\.build\bin\all\bsc.exe --log .output/.new/.log --out .output/.new 
-..\..\.build\bin\all\bsc.exe --log .output/.old/.log --out .output/.old --set-OLDONLY --set-USEALL
-..\..\.build\bin\all\bsc.exe --log .output/.all/.log --out .output/.all --set-USEALL
+..\..\.build\bin\all\bsc.exe --log .output/.new/.log --out .output/.new  --loglevel Info
+..\..\.build\bin\all\bsc.exe --log .output/.old/.log --out .output/.old --set-OLDONLY --set-USEALL --loglevel Info
+..\..\.build\bin\all\bsc.exe --log .output/.all/.log --out .output/.all --set-USEALL --loglevel Info
+..\..\.build\bin\all\bsc.exe --log .output/.demo/.log --out .output/.demo --set-DEMO --loglevel Info
 pause
