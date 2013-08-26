12d42ba099b6228fb60cb8e82c4f243320db6a1c
comdiv <fagim.sadykov@gmail.com>
2013-08-26 16:21:13 +0600

    fixes to Z4

--- zeta.data/compile.bat
+++ zeta.data/compile.bat
@@ -1,4 +1,4 @@
-..\..\.build\bin\all\bsc.exe --log .output/.new/.log --out .output/.new  --loglevel Error
+..\..\.build\bin\all\bsc.exe --log .output/.new/.log --out .output/.new  --loglevel All
 ..\..\.build\bin\all\bsc.exe --log .output/.old/.log --out .output/.old --set-OLDONLY --set-USEALL --loglevel Error
 ..\..\.build\bin\all\bsc.exe --log .output/.all/.log --out .output/.all --set-USEALL --loglevel Error
 ..\..\.build\bin\all\bsc.exe --log .output/.demo/.log --out .output/.demo --set-DEMO --loglevel Error
