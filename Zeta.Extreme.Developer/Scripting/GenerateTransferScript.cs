4a689d7d272f16500afbeafe8d21e4967f58e318
comdiv <fagim.sadykov@gmail.com>
2013-09-12 10:55:58 +0600

    doc_fixes

---
+++ Zeta.Extreme.Developer/Scripting/GenerateTransferScript.cs
@@ -0,0 +1,53 @@
+﻿using Qorpent.Applications;
+using Qorpent.Utils.Extensions;
+
+namespace Zeta.Extreme.Developer.Scripting {
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GenerateTransferScript : ScriptCommandBase
+    {
+        private string @from;
+        private string to;
+        private string col;
+        private string year;
+        private string period;
+        private string obj;
+        private string usr;
+
+        /// <summary>
+        /// Просто вызывает стандартный экспорт периодов
+        /// </summary>
+        /// <returns></returns>
+        protected override string GetCommandName()
+        {
+            return ScriptConstants.TRANSFER_DATA_COMMAND;
+        }
+
+        /// <summary>
+        /// Инициализатор
+        /// </summary>
+        /// <param name="def"></param>
+        public override void Initialize(System.Xml.Linq.XElement def)
+        {
+            base.Initialize(def);
+            from = def.Attr("from");
+            to = def.Attr("to");
+            col = def.Attr("col");
+            year = def.Attr("year");
+            period = def.Attr("period");
+            obj = def.Attr("obj");
+            usr = "transfer@"+from+"@"+ Application.Current.Principal.CurrentUser.Identity.Name;
+        }
+
+        /// <summary>
+        /// Сформировать параметры запроса
+        /// </summary>
+        /// <param name="commandName"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        protected override object SetupParameters(string commandName, Qorpent.Config.IConfig context) {
+            return new { from, to, col, year, period, obj, usr };
+        }
+    }
+}
---
+++ Zeta.Extreme.Developer/Scripting/ScriptConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace Zeta.Extreme.Developer.Scripting {
+    /// <summary>
+    /// Константы скриптов
+    /// </summary>
+    public static class ScriptConstants {
+        /// <summary>
+        /// Комманда экспорта форм
+        /// </summary>
+        public const string GENERATE_FORM_COMMAND = "zdev/exporttree";
+
+        /// <summary>
+        /// Команда зависимостей формы
+        /// </summary>
+        public const string FORM_DEPENDENCY_COMMAND = "zdev/exportdependencydot";
+        /// <summary>
+        /// Команда экспорта периодов
+        /// </summary>
+        public const string EXPORT_PERIODS_COMMAND = "zdev/exportperiods";
+        /// <summary>
+        /// Команда переноса данных
+        /// </summary>
+        public const string TRANSFER_DATA_COMMAND = "zdev/transferdata";
+    }
+}
