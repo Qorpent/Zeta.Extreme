f4ca9caf0805b2815d3ad5818c504f04621e47e2
comdiv <fagim.sadykov@gmail.com>
2013-10-09 19:15:51 +0600

    #AF-825 Fixed

---
+++ Zeta.Extreme.Core.Tests/NativeMaping/MiniHoldingHelperTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Qorpent.Applications;
+using Qorpent.Mvc;
+using Qorpent.Security;
+using Zeta.Extreme.Core.Tests.CoreTests;
+using Zeta.Extreme.Form.Meta;
+using Zeta.Extreme.Model;
+using Zeta.Extreme.Model.Inerfaces;
+using Zeta.Extreme.Model.MetaCaches;
+using Zeta.Extreme.Model.SqlSupport;
+
+namespace Zeta.Extreme.Core.Tests.NativeMaping
+{
+    [TestFixture]
+    public class MiniHoldingHelperTests : DatabaseAwaredTestBase
+    {
+        [TestCase("OCM", true)]
+        [TestCase("AGRO", true)]
+        [TestCase("NAUKA", false)]
+        public void DivisionIsMiniholdingDetection(string divisionCode, bool result) {
+            Assert.AreEqual(result, ObjCache.DivByCode[divisionCode].IsMiniholding());
+        }
+
+        [TestCase("OCM", 1046)]
+        [TestCase("AGRO", 527)]
+        [TestCase("NAUKA", 0)]
+        public void ManageCompanyIdDetectedForDivision(string divisionCode, int result)
+        {
+            Assert.AreEqual(result, ObjCache.DivByCode[divisionCode].GetManageCompanyId());
+        }
+
+        [Test]
+        public void CanRetrieveManageCompany() {
+            var uc = ObjCache.DivByCode["NAUKA"].GetManageCompany();
+            Assert.Null(uc);
+            uc = ObjCache.DivByCode["OCM"].GetManageCompany();
+            Assert.NotNull(uc);
+            Assert.AreEqual(1046,uc.Id);
+        }
+
+        class StubRR : IRoleResolver
+        {
+
+
+            public bool IsInRole(IPrincipal principal, string role, bool exact = false, IMvcContext callcontext = null,
+                                 object customcontext = null)
+            {
+                return role == MiniholdingHelper.AllMiniholdingRole;
+            }
+
+            public bool IsInRole(string username, string role, bool exact = false, IMvcContext callcontext = null,
+                                 object customcontext = null)
+            {
+                return role == MiniholdingHelper.AllMiniholdingRole;
+            }
+
+            public IEnumerable<string> GetRoles(IPrincipal principal, IMvcContext callcontext = null, object customcontext = null)
+            {
+                yield return MiniholdingHelper.AllMiniholdingRole;
+            }
+        }
+        /// <summary>
+        /// Проверяем работу роли "весь минихолдинг"
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public void CanCheckAllMiniholdingRole() {
+            var currentRoles = Application.Current.Roles;
+            var currentHook = NativeZetaReader.DebugHook;
+            try {
+                 Application.Current.Roles  = new StubRR();
+                NativeZetaReader.DebugHook = OnNativeZetaHook;
+                var user = new GenericPrincipal(new GenericIdentity("best\\test"), null);
+                Assert.True(ObjCache.Get(1046).Authorize(user));
+                Assert.True(ObjCache.Get(415).Authorize(user));
+                Assert.True(ObjCache.Get(468).Authorize(user));
+                Assert.False(ObjCache.Get(352).Authorize(user));
+
+            }
+            finally {
+                Application.Current.Roles = currentRoles;
+                NativeZetaReader.DebugHook = currentHook;
+
+            }
+
+        }
+
+        private static IEnumerable OnNativeZetaHook(string _)
+        {
+            if (_.Contains("Login"))
+            {
+                yield return new User { Object = ObjCache.Get(1046) };
+            }
+        }
+    }
+}
---
+++ Zeta.Extreme.Model/MiniholdingHelper.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Principal;
+using Qorpent.Applications;
+using Qorpent.Mvc;
+using Qorpent.Security;
+using Qorpent.Utils.Extensions;
+using Zeta.Extreme.Model.Inerfaces;
+using Zeta.Extreme.Model.MetaCaches;
+using Zeta.Extreme.Model.SqlSupport;
+
+namespace Zeta.Extreme.Model
+{
+    /// <summary>
+    /// Вспомогательный класс для работы с минихолдингами
+    /// </summary>
+    public static class MiniholdingHelper {
+        /// <summary>
+        /// Имя тега управляющей компании
+        /// </summary>
+        public const string ManageCompanyTag = "uc";
+        /// <summary>
+        /// Имя роли минихолдина
+        /// </summary>
+        public const string AllMiniholdingRole = "ALL_MINIHOLDING";
+
+        /// <summary>
+       /// True если DIV имеет пометку тегом uc
+       /// </summary>
+       /// <param name="division"></param>
+       /// <returns></returns>
+       public static bool IsMiniholding(this IObjectDivision division) {
+           return 0 != division.TagGet(ManageCompanyTag).ToInt();
+       }
+        /// <summary>
+        /// Возвращает идентификатор
+        /// </summary>
+        /// <param name="division"></param>
+        /// <returns></returns>
+        public static int GetManageCompanyId(this IObjectDivision division) {
+            return division.TagGet(ManageCompanyTag).ToInt();
+        }
+        /// <summary>
+        /// Возвращает ID управляющей компании для отдельного объекта
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static int GetManageCompanyId(this IZetaMainObject obj) {
+            if (null == obj.Division) return 0;
+            return obj.Division.GetManageCompanyId();
+        }
+        /// <summary>
+        /// Проверяет, является ли объект управляющей компанией
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool IsManageCompany(this IZetaMainObject obj) {
+            return obj.GetManageCompanyId() == obj.Id;
+        }
+        /// <summary>
+        /// Получить объект управляющей компании
+        /// </summary>
+        /// <param name="div"></param>
+        /// <returns></returns>
+        public static IZetaMainObject GetManageCompany(this IObjectDivision div)
+        {
+            var mcid = div.GetManageCompanyId();
+            if (0 == mcid) return null;
+            return MetaCache.Default.Get<IZetaMainObject>(mcid);
+        }
+        /// <summary>
+        /// Получить объект управляющей компании
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static IZetaMainObject GetManageCompany(this IZetaMainObject obj) {
+            var mcid = obj.GetManageCompanyId();
+            if (0 == mcid) return null;
+            if (obj.Id == mcid) return obj;
+            return MetaCache.Default.Get<IZetaMainObject>(mcid);
+        }
+       
+    }
+}
