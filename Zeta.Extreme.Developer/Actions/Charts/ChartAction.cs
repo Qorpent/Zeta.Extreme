c3ca4f1a0a60b604f992bb0e73c06f0dae163658
Kirill Shirshov <remalloc@gmail.com>
2013-10-11 12:46:42 +0600

    Refactored

--- Zeta.Extreme.Developer/Actions/Charts/ChartAction.cs
+++ Zeta.Extreme.Developer/Actions/Charts/ChartAction.cs
@@ -17,9 +17,16 @@ namespace Zeta.Extreme.Developer.Actions.Charts {
         /// <returns></returns>
         protected override object GenerateChart(int year, int period, int obj) {
             return new Chart()
+                .SetXAxisName("Периоды")
+                .SetYAxisName("Котировки")
                 .SetCaption("Котировки цинка на LME")
                 .SetSubCaption("$ за тонну ZN")
                 .SetCaptionPadding(10)
+                .SetBgColor("FFFFFF,FFFFFF")
+                .SetDivIntervalHints("1600,1700,1800,1900,2000,2100,2200,2300")
+                .SetAlpha(0)
+                .SetShowAlternateHGridColor(false)
+                .SetDivLineAlpha(0)
                 .SetConfig(
                     new ChartConfig().SetChartType(FusionChartType.MSLine))
                 .Add(
@@ -36,7 +43,7 @@ namespace Zeta.Extreme.Developer.Actions.Charts {
                         .SetColor("424242"))
                 .Add(
                     new ChartTrendLine()
-                        .SetStartValue(1750.0)
+                        .SetStartValue(1790.0)
                         .SetColor("FF0000")
                         .SetDashed(true)
                         .SetDisplayValue("ТПФП"))
